79062295546f9284c54677ce0a3327f890370002
Akshaya Kumaran <akshayakumaran13@gmail.com>
2026-06-07 13:36:23 +0530

    Fix HF port

--- app.py
+++ app.py
@@ -173,6 +173,4 @@ def movie_page(movie_id):
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=7860)
